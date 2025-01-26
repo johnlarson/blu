d4f94f362ed0a2b10110a77549abad7f3906ef56
John Larson <john.robert.larson@gmail.com>
2025-01-25 20:42:08 -0700

    implement default app

--- src/blu/_default_app.py
+++ src/blu/_default_app.py
@@ -1,5 +1,31 @@
 from blu._utils import asgi
 
 
-async def app(scope: asgi.Scope, receive: asgi.Receiver, send: asgi.Sender):
-    ...
+import functools
+import os
+from blu._app import Blu
+from blu._utils import asgi
+
+
+class DefaultApp:
+    _project_dir: str
+
+    def __init__(self):
+        self._project_dir = os.getcwd()
+
+    async def __call__(
+        self,
+        scope: asgi.Scope,
+        receive: asgi.Receiver,
+        send: asgi.Sender,
+    ):
+        inner_app = self._get_inner_app()
+        return await inner_app(scope, receive, send)
+    
+    @functools.cache
+    def _get_inner_app(self) -> asgi.App:
+        import app  # type: ignore
+        return Blu(app, self._project_dir)
+    
+
+app = DefaultApp()

de9de2451b9b087752d4cc9e63c8025baf63410c
John Larson <john.robert.larson@gmail.com>
2025-01-26 19:11:40 -0700

    progress

--- src/blu/_app/asgi_app/router.py
+++ src/blu/_app/asgi_app/router.py
@@ -1,10 +1,24 @@
+from collections.abc import Callable
+from typing import Any, Protocol
 from blu._http import Request, Response
 
 
+class AnyCallable(Protocol):
+    def __call__(self, *args: Any, **kwargs: Any) -> Any: ...
+
+
+class RouteNotFound(Exception):
+    pass
+
+
 class Router:
-    
+    _index_page: AnyCallable
+    _default_page: AnyCallable
+    _static_segments: dict[str, 'Router']
+    _dynamic_segments: list['Router']
+
     def __init__(self, app_module: str):
         ...
 
-    async def handle(self, request: Request) -> Response:
+    def handle(self, request: Request) -> Response:
         ...

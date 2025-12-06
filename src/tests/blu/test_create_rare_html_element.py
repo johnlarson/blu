c4fe065e2531430da654e43ff4a6d0c1ea19037f
John Larson <john.robert.larson@gmail.com>
2025-12-05 21:07:43 -0700

    create_rare_html_element test descriptions

---
+++ src/tests/blu/test_create_rare_html_element.py
@@ -0,0 +1,16 @@
+def test_create_element():
+    """
+    Returns an HTMLElement whose tagname exactly matches the provided
+    tag name.
+    """
+    ...
+
+
+def test_no_props():
+    """The HTMLElement returned has no props."""
+    ...
+
+
+def test_no_children():
+    """The HTMLElement returned has no children."""
+    ...

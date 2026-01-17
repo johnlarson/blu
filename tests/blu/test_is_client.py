554a274ad06e549279a58a4d2e9024c7a4ee452f
John Larson <john.robert.larson@gmail.com>
2026-01-16 20:02:35 -0700

    test_is_client

--- tests/blu/test_is_client.py
+++ tests/blu/test_is_client.py
@@ -1,8 +1,28 @@
+from contextlib import contextmanager
+import importlib
+import platform
+from unittest import mock
+
+import blu
+from blu import _utils
+from blu._utils import client as client_utils
+
+
+@contextmanager
+def reload_is_client(system):
+    with mock.patch('platform.system', return_value=system):
+        importlib.reload(client_utils)
+        importlib.reload(_utils)
+        importlib.reload(blu)
+        yield
+
+
 def test_is_false_in_server_environment():
     """blu.is_client evaluates to False in a server environment."""
-    ...
+    assert not blu.is_client
 
 
 def test_is_true_in_client_environment():
     """blu.is_client evaluates to True in a client environment."""
-    ...
+    with reload_is_client('Emscripten'):
+        assert blu.is_client

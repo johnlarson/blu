9a0f8594b4d0c00a0ba1dcbc67346acd005b0885
John Larson <john.robert.larson@gmail.com>
2026-01-16 21:05:11 -0700

    test_router

---
+++ tests/file_conventions/_utils.py
@@ -0,0 +1,7 @@
+import importlib
+from blu._app.router import Router, router_from_root_package
+
+
+def test_router(module_name: str) -> Router:
+    module = importlib.import_module(f'tests.apps.{module_name}')
+    return router_from_root_package(module)

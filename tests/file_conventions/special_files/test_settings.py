a97948c3fedeff528918f71a9076cc44784e08b0
John Larson <john.robert.larson@gmail.com>
2026-01-20 21:57:20 -0700

    start writing settings tests

--- tests/file_conventions/special_files/test_settings.py
+++ tests/file_conventions/special_files/test_settings.py
@@ -1,29 +1,22 @@
-def test_no_settings_file():
+from blu._http import Request
+from tests.file_conventions._utils import router
+
+
+async def test_no_settings_file():
     """Application runs with no __settings__.py file."""
-    ...
+    r = router('basic')
+    response = await r.handle(Request('/'))
+    assert response._body == 'Hello, World!'  # type: ignore
 
 
-def test_ignore_settings_file_outside_app_directory():
-    """
-    A __settings__.py file outside the app package will be ignored.
-    """
-    ...
-
-
-def test_ignore_settings_file_below_top_level():
-    """
-    A __settings__.py file below the top level of the app package is
-    ignored.
-    """
-    ...
-
-
-def empty_settings_file():
+async def test_empty_settings_file():
     """Application runs if __settings__.py file is empty."""
-    ...
+    r = router('empty_settings')
+    response = await r.handle(Request('/'))
+    assert response._body == 'Hello, World!'  # type: ignore
 
 
-def test_CLIENT_REQUIREMENTS_one_requirement():
+async def test_CLIENT_REQUIREMENTS_one_requirement():
     """
     If the CLIENT_REQUIREMENTS is a list with one package name, that
     package will be available to client-side code.
@@ -31,9 +24,14 @@ def test_CLIENT_REQUIREMENTS_one_requirement():
     ...
 
 
-def test_CLIENT_REQUIRMENTS_multiple_requirements():
+async def test_CLIENT_REQUIRMENTS_multiple_requirements():
     """
     If the CLIENT_REQUIREMENTS is a list with multiple package names,
     those packages will all be available to client-side code.
     """
+    ...
+
+
+async def test_CLIENT_REQUIREMENTS_default():
+    """CLIENT_REQUIREMENTS defaults to an empty list."""
     ...

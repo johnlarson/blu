407bc12cab83d73d293dbabdacb55fb3d0fb04a9
John Larson <john.robert.larson@gmail.com>
2026-01-23 21:34:58 -0700

    sketch out cli tests

--- tests/cli/test_build.py
+++
@@ -1,8 +0,0 @@
-def test_copies_static_files():
-    """
-    When `blu build` is run from the command line in the project root
-    directory (the parent of the app directory), all non-python files
-    are copied over to <root directory>/static, following the same
-    directory structure that they had under <root directory>/app.
-    """
-    ...
---
+++ tests/test_cli.py
@@ -0,0 +1,11 @@
+def test_dev_server():
+    """
+    When a `blu` is run, a dev server starts up that serves the app on a
+    port that is given by the output to `blu dev`.
+    """
+    ...
+
+
+def test_reload_on_change():
+    """The dev server reloads when a Python file is changed."""
+    ...

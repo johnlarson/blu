3a702741b1ad20c23ae7f3bafb9d05a1d05f4ea9
John Larson <john.robert.larson@gmail.com>
2026-01-24 17:38:45 -0700

    sketch out end-to-end tests

---
+++ tests/test_e2e.py
@@ -0,0 +1,49 @@
+
+
+
+async def test_render_nodes():
+    ...
+
+
+async def test_routing():
+    ...
+
+
+async def test_is_client():
+    ...
+
+
+async def test_wrong_environment_error():
+    ...
+
+
+async def test_use_effect():
+    ...
+
+
+async def test_use_ref():
+    ...
+
+
+async def test_use_state():
+    ...
+
+
+async def test_use_ref_html_element():
+    ...
+
+
+async def test_client_side_availability():
+    ...
+
+
+async def test_static_files():
+    ...
+
+
+async def test_client_file_specifier():
+    ...
+
+
+async def test_dev_server():
+    ...

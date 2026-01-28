cf38b53fa11edf831a8ff9a30062749c911c73b6
John Larson <john.robert.larson@gmail.com>
2026-01-27 22:19:00 -0700

    use_state test app

---
+++ tests/apps/e2e/use_state/__index__.py
@@ -0,0 +1,21 @@
+from blu import client, use_state
+from blu.html import button, div
+
+__client__ = True
+
+
+def __page__():
+    return StateTest
+
+
+@client
+def StateTest():
+    count, set_count = use_state(0)
+
+    def click_increment():
+        set_count(count + 1)
+    
+    return (
+        div(id='count')[count],
+        button(id='increment', onClick=click_increment)['Increment'],
+    )

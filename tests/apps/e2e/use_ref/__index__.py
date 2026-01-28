d7aef0d7ee53382bf88fd98e1294b0ffe01bcf70
John Larson <john.robert.larson@gmail.com>
2026-01-27 22:16:12 -0700

    use_ref test app

---
+++ tests/apps/e2e/use_ref/__index__.py
@@ -0,0 +1,27 @@
+import uuid
+from blu import HTMLElement, Ref, client, use_effect, use_ref, use_state
+from blu.html import button, div
+
+__client__ = True
+
+
+def __page__():
+    return RefTest
+
+
+@client
+def RefTest():
+    render_id, rerender = use_state(0)
+    count_ref = use_ref(0)
+
+    def click_increment():
+        count_ref[:] = count_ref[:] + 1
+
+    def click_rerender():
+        rerender(render_id + 1)
+    
+    return (
+        div(id='count')[count_ref[:]],
+        button(id='increment', onClick=click_increment)['Increment'],
+        button(id='rerender', onClick=click_rerender)['Rerender'],
+    )

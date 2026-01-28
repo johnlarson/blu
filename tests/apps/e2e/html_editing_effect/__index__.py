a6305d2b29a3117c2ab4e8619f6a1b99e65e592c
John Larson <john.robert.larson@gmail.com>
2026-01-27 22:25:31 -0700

    html_editing_effect test app

---
+++ tests/apps/e2e/html_editing_effect/__index__.py
@@ -0,0 +1,20 @@
+import uuid
+from blu import HTMLElement, Ref, client, use_effect, use_ref, use_state
+from blu.html import button, div
+
+__client__ = True
+
+
+def __page__():
+    return RefTest
+
+
+@client
+def RefTest():
+    div_ref = use_ref(None)
+
+    @use_effect
+    def _():
+        div_ref[:].innerText = 'Hello.'
+    
+    return div(id='test-div', ref=div_ref)

ae92494001d322d51b5dd833fc0bb8793e6145ee
John Larson <john.robert.larson@gmail.com>
2026-03-06 20:41:27 -0700

    render_root

--- src/blu/util.js
+++ src/blu/util.js
@@ -1,6 +1,16 @@
+import { createElement as $ } from 'https://esm.sh/react';
+import { createRoot } from 'https://esm.sh/react-dom/client';
+
 console.log('Hello from client utils!');
 
 export function render_root(py_root) {
+  const jsNode = getReactNode(py_root);
+  const jsRoot = isValidRootNode(jsNode) ? jsNode : $('div', null, jsNode);
+  const renderTarget = jsRoot.type == 'html' ? document : document.body;
+  createRoot(renderTarget).render(jsRoot);
+}
+
+function getReactNode(pyNode) {
 
 }
 
@@ -13,5 +23,5 @@ export function use_ref(init) {
 }
 
 export function use_effect(callback) {
-  
+
 }

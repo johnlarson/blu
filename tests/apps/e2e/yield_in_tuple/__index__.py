7df47081247fc090ca1485120e27e6b5d1335478
John Larson <john.robert.larson@gmail.com>
2026-03-21 11:29:42 -0600

    progress

---
+++ tests/apps/e2e/yield_in_tuple/__index__.py
@@ -0,0 +1,13 @@
+from blu import client
+from blu.html import span
+
+__client__ = True
+
+
+def __page__():
+    return ColoredText("red")["This should be red.",]
+
+
+@client
+def ColoredText(color):
+    return span(style={"color": color})[((yield),)]

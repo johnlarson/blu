7b3eb0dd8edef7f028137cdc25f963e081415dbf
John Larson <john.robert.larson@gmail.com>
2026-03-25 21:12:33 -0600

    use tmp_path fixture

--- tests/blu/test_security.py
+++ tests/blu/test_security.py
@@ -16,16 +16,14 @@ async def test_server_functions_no_expose_server_only_modules(
     """
     c = await client("e2e")
     response = await c.get("/_blu_internal/app_pkg.zip")
-    with TemporaryDirectory() as temp_dir_str:
-        temp_dir = Path(temp_dir_str)
-        zip_path = temp_dir / "app_pkg.zip"
-        with open(zip_path, "wb") as zip_f_write:
-            async for chunk in response.content.iter_chunked(1024):
-                zip_f_write.write(chunk)
-        with ZipFile(zip_path, "r") as zip_f_read:
-            zip_f_read.extractall(temp_dir)
-        with open(temp_dir / "server_functions/hello_module.py", "r") as f:
-            data = f.read()
+    zip_path = tmp_path / "app_pkg.zip"
+    with open(zip_path, "wb") as zip_f_write:
+        async for chunk in response.content.iter_chunked(1024):
+            zip_f_write.write(chunk)
+    with ZipFile(zip_path, "r") as zip_f_read:
+        zip_f_read.extractall(tmp_path)
+    with open(tmp_path / "server_functions/hello_module.py", "r") as f:
+        data = f.read()
     assert "def hello(" in data
     assert "@server" in data
     assert "..." in data
@@ -34,7 +32,7 @@ async def test_server_functions_no_expose_server_only_modules(
     assert "return " not in data
 
 
-async def test_no_access_non_client_files(client: ClientFixture):
+async def test_no_access_non_client_files(client: ClientFixture, tmp_path: Path):
     """
     Python files within the app package are not accessible from outside
     the server unless they contain the top-level statement
@@ -42,15 +40,13 @@ async def test_no_access_non_client_files(client: ClientFixture):
     """
     c = await client("e2e")
     response = await c.get("/_blu_internal/app_pkg.zip")
-    with TemporaryDirectory() as temp_dir_str:
-        temp_dir = Path(temp_dir_str)
-        zip_path = temp_dir / "app_pkg.zip"
-        with open(zip_path, "wb") as zip_f_write:
-            async for chunk in response.content.iter_chunked(1024):
-                zip_f_write.write(chunk)
-        with ZipFile(zip_path, "r") as zip_f_read:
-            zip_f_read.extractall(temp_dir)
-        success_path = temp_dir / "app_pkg_clientside/success/module.py"
-        assert success_path.exists()
-        fail_path = temp_dir / "app_pkg_clientside/fail/module.py"
-        assert not fail_path.exists()
+    zip_path = tmp_path / "app_pkg.zip"
+    with open(zip_path, "wb") as zip_f_write:
+        async for chunk in response.content.iter_chunked(1024):
+            zip_f_write.write(chunk)
+    with ZipFile(zip_path, "r") as zip_f_read:
+        zip_f_read.extractall(tmp_path)
+    success_path = tmp_path / "app_pkg_clientside/success/module.py"
+    assert success_path.exists()
+    fail_path = tmp_path / "app_pkg_clientside/fail/module.py"
+    assert not fail_path.exists()
